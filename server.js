33dff29c364d087aa11852e0929c18326ca113aa
Andy Nguyen <Ydna317@gmail.com>
2021-04-18 18:26:56 -0700

    connecting to mongo db atlas and deploying with heroku.

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -15,10 +25,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
